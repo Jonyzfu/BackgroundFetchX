5ae697a67e79623b4a1cc2729c6d7db98e5604a7
jonyzfu <dane.radar@gmail.com>
2015-03-17 10:04:38 -0700

    Setup the interface and refresh control

--- BackgroundFetchDemo/ViewController.m
+++ BackgroundFetchDemo/ViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ViewController.h"
+#define NewsFeed @"http://feeds.reuters.com/reuters/technologyNews"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) UIRefreshControl *refreshControl;
+
+@property (nonatomic, strong) NSArray *arrNewsData;
+
+@property (nonatomic, strong) NSString *dataFilePath;
 
 @end
 
@@ -18,6 +25,22 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // 1. Make self the delegate and datasource of the table view.
+    [self.tblNews setDelegate:self];
+    [self.tblNews setDataSource:self];
+    
+    // 2. Specify the data storage file path.
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *docDirectory = [paths objectAtIndex:0];
+    self.dataFilePath = [docDirectory stringByAppendingPathComponent:@"newsdata"];
+    
+    // 3. Initialize the refresh control.
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self
+                            action:@selector(refreshData)
+                  forControlEvents:UIControlEventValueChanged];
+    [self.tblNews addSubview:self.refreshControl];
 }
 
 - (void)didReceiveMemoryWarning
