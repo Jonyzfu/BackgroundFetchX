1d6774d18f29dc1b59090b8f7314ab7cee860c70
jonyzfu <dane.radar@gmail.com>
2015-03-17 11:29:15 -0700

    Refreshing and displaying the data

--- BackgroundFetchDemo/ViewController.m
+++ BackgroundFetchDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "XMLParser.h"
 #define NewsFeed @"http://feeds.reuters.com/reuters/technologyNews"
 
 @interface ViewController ()
@@ -17,9 +18,86 @@
 
 @property (nonatomic, strong) NSString *dataFilePath;
 
+- (void)refreshData;
+
+- (void)performNewFetchedDataActionsWithDataArray:(NSArray *)dataArray;
+
 @end
 
 @implementation ViewController
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.arrNewsData.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"idCellNewsTitle"];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"idCellNewsTitle"];
+    }
+    
+    NSDictionary *dict = [self.arrNewsData objectAtIndex:indexPath.row];
+    
+    cell.textLabel.text = [dict objectForKey:@"title"];
+    cell.detailTextLabel.text = [dict objectForKey:@"pubDate"];
+    
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 80.0;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSDictionary *dict = [self.arrNewsData objectAtIndex:indexPath.row];
+    
+    NSString *newsLink = [dict objectForKey:@"link"];
+    
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:newsLink]];
+}
+
+
+- (void)refreshData
+{
+    XMLParser *xmlParser = [[XMLParser alloc] initWithXMLURLString:NewsFeed];
+    [xmlParser startParsingWithCompletionHandler:^(BOOL success, NSArray *dataArray, NSError *error) {
+        if (success) {
+            [self performNewFetchedDataActionsWithDataArray:dataArray];
+            
+            [self.refreshControl endRefreshing];
+        } else {
+            NSLog(@"%@", [error localizedDescription]);
+        }
+    }];
+}
+
+- (void)performNewFetchedDataActionsWithDataArray:(NSArray *)dataArray
+{
+    // 1. Initialize the arrNewsData array with the parsed data array.
+    if (self.arrNewsData != nil) {
+        self.arrNewsData = nil;
+    }
+    self.arrNewsData = [[NSArray alloc] initWithArray:dataArray];
+    
+    // 2. Reload the table view.
+    [self.tblNews reloadData];
+    
+    // 3. Save the data permanently to file.
+    if (![self.arrNewsData writeToFile:self.dataFilePath atomically:YES]) {
+        NSLog(@"Counldn't save data.");
+    }
+}
+
 
 - (void)viewDidLoad
 {
